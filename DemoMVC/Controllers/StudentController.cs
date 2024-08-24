61c1d6cdb44cc018f07e921e6fc44117530273dc
trung603 <lethetrung2@gmail.com>
2024-08-24 14:27:17 -0700

    DemoMVC

---
+++ DemoMVC/Controllers/StudentController.cs
@@ -0,0 +1,12 @@
+
+using Microsoft.AspNetCore.Mvc;
+
+
+namespace DemoMVC.Controllers;
+public class StudentController : Controller
+{
+    public IActionResult Index1()
+    {
+        return View();
+    }
+}
