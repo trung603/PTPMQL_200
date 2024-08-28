d4fe15becf13ae2ad2d774f631ecc63b90e79273
trung603 <lethetrung2@gmail.com>
2024-08-27 21:11:02 -0700

    HTML Done!

--- DemoMVC/obj/Debug/net8.0/DemoMVC.AssemblyInfo.cs
+++ DemoMVC/obj/Debug/net8.0/DemoMVC.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("DemoMVC")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3fd2b0e45a31db724a7f7f20075668286537cec8")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+dbdc53e8378f8cba641d636967a37d6b07c3dc26")]
 [assembly: System.Reflection.AssemblyProductAttribute("DemoMVC")]
 [assembly: System.Reflection.AssemblyTitleAttribute("DemoMVC")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
